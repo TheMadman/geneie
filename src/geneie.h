1de6581d294575a04be7f265e654f4de45292ce6
Marcus Harrison <marcus@harrisonland.co.uk>
2024-06-24 20:43:28 +0200

    Add geneie.h

---
+++ src/geneie.h
@@ -0,0 +1,28 @@
+/*
+ * Geneie - A Library and Tools for gene processing
+ * Copyright (C) 2024   Marcus Harrison
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GENEIE_H
+#define GENEIE_H
+
+#include "geneie/code.h"
+#include "geneie/sequence.h"
+#include "geneie/sequence_ref.h"
+#include "geneie/encoding.h"
+#include "geneie/sequence_tools.h"
+
+#endif // GENEIE_H
